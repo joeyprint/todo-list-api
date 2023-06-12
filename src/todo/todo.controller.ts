e4813bd3a1d54bc6087ba50be421a214e24b5461
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-06-12 22:18:25 +0700

    feat: add in memory feature and update create and get all todo list

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -10,33 +10,45 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoEntity } from './entities/todo.entity';
+import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
+import { randomUUID } from 'crypto';
 
 @Controller('todo')
 export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(private readonly todoService: InMemoryDBService<TodoEntity>) {}
 
   @Post()
-  create(@Body() createTodoDto: CreateTodoDto) {
-    return this.todoService.create(createTodoDto);
+  create(@Body() createTodoDto: CreateTodoDto): TodoEntity {
+    const createdAt = new Date().toISOString();
+
+    const mappedTodo = {
+      id: randomUUID(),
+      title: createTodoDto.title,
+      createdAt,
+      updatedAt: createdAt,
+    };
+
+    return this.todoService.create(mappedTodo);
   }
 
   @Get()
-  findAll() {
-    return this.todoService.findAll();
+  findAll(): Array<TodoEntity> {
+    return this.todoService.getAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.todoService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.todoService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.todoService.update(+id, updateTodoDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+  //   return this.todoService.update(+id, updateTodoDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.todoService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.todoService.remove(+id);
+  // }
 }
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [TodoService],
+  imports: [InMemoryDBModule.forFeature('todo', {})],
 })
 export class TodoModule {}
