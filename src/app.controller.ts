689eb394e4a7570be42dd3c2ad53ae5d02ab64cd
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-06-12 22:17:09 +0700

    feat: remove unused code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Todo } from './todo/entities/todo.entity';
 
 @Controller()
 export class AppController {
