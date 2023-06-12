c2bed140a6479da5be10276f83bf11450bedd8fd
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-06-12 22:17:36 +0700

    feat: create interface of todo entity

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,1 +1,8 @@
-export class Todo {}
+import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
+
+export interface TodoEntity extends InMemoryDBEntity {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
